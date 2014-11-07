05c679e774def1c8981fc647044d3c04eba6e163
Jeff Hui & Serguei Filimonov <pair+jhui+sfilimonov@pivotal.io>
2014-11-07 15:26:19 -0800

    improve the download and analysys a bit

--- src/panel/downloader.rb
+++ src/panel/downloader.rb
@@ -1,6 +1,4 @@
-
-
-ARGV.each do |ip_of_nats_client|
+ARGV.map do |ip_of_nats_client|
   puts "--> downloading #{ip_of_nats_client}:4567/download_logs > logs/#{ip_of_nats_client}.log"
   `curl -L 'http://#{ip_of_nats_client}:4567/download_logs' > logs/#{ip_of_nats_client}.log`
 end
