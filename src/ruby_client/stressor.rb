0fc00dbe4ac1b21ed96681b95c605d97e347fb17
Jeff Hui and Serguei Filimonov <sfilimonov@pivotal.io>
2014-11-12 15:44:34 -0800

    Add packet loss instrumentation and datadog
    - Fixed go client monit stop routines to actually stop running.
    - add datadog metric reports of sent, compeleted and outstanding
    messages

--- src/ruby_client/stressor.rb
+++ src/ruby_client/stressor.rb
@@ -1,36 +1,73 @@
 class NATSStressor
-  def initialize(client, logger, name, payload_size)
+  def initialize(client, logger, name, payload_size, population, api_key, storage_file)
     @client = client
     @logger = logger
     @name = name
     @payload_size = payload_size
-    @msg_counter = 1
-    @received = {}
+    @msg_counter = 0
+    @message_tally = {}
+    @msgs_completed = 0
+    @population = population
+    @api_key = api_key
+    @storage_file = storage_file
   end
 
   def start
     @client.subscribe(">") do |message, reply, subject|
-      type, name, _ = message.split('--')
-      if type == 'publish'
-        @received[name] ||= 0
-        @received[name] += 1
-        @logger.info(@received.map { |k, v| "#{k}: #{v}" }.join(' '))
+      if message =~ /^publish--/
+        @client.publish("ruby.publish", "received_publish--#{@name}--#{message}")
       end
-      # @logger.info("receiving " + message)
+      if message =~ /^received_publish--.*?--publish--#{@name}/
+        original_msg = message[/^received_publish--(.*?)--(.*)/, 2]
+        puts "                : #{message}"
+        puts "original_msg rec: #{original_msg}"
+        recepient = message[/^received_publish--(.*?)--(.*)/, 1]
+        @message_tally[original_msg] ||= []
+        @message_tally[original_msg] << recepient
+        if @message_tally[original_msg].uniq.size == @population
+          @logger.info "tallied up #{@message_tally[original_msg].join(",")}"
+          @msgs_completed += 1
+          @message_tally.delete(original_msg)
+        end
+      end
     end
   end
 
   def perform_interactions
-    request_msg = "request--#{@name}--#{@msg_counter}--" + "."*@payload_size
+    #request_msg = "request--#{@name}--#{@msg_counter}--" + "."*@payload_size
     publish_msg = "publish--#{@name}--#{@msg_counter}--" + "."*@payload_size
 
     @logger.info("publishing " + publish_msg)
+    @logger.info("completed #{@msgs_completed} messsages. outstanding: #{@message_tally.keys.size}")
+    datadog("msgs.sent", @msg_counter)
+    datadog("msgs.completed", @msgs_completed)
+    datadog("msgs.outstanding", @message_tally.keys.size)
+    puts "original_msg pub: #{publish_msg}"
     @client.publish("ruby.publish", publish_msg)
+    @message_tally[publish_msg] = []
 
-    @logger.info("requesting " + request_msg)
-    @client.request("ruby.request", request_msg) do |response|
-      @logger.info("receiving_response #{request_msg} #{response}")
-    end
+    File.write(@storage_file, JSON.pretty_generate(@message_tally))
+    #@logger.info("requesting " + request_msg)
+    #@client.request("ruby.request", request_msg) do |response|
+      #@logger.info("receiving_response #{request_msg} #{response}")
+    #end
     @msg_counter += 1
+  end
+
+  private
+  def datadog(metric, value)
+    curl = <<-BASH
+      curl -s -X POST -H "Content-type: application/json" \
+      -d '{ "series" :
+               [{"metric":"nats-stress.#{metric}",
+                "points":[[#{Time.now.utc.to_i}, #{value}]],
+                "type":"gauge",
+                "host":"#{@name}",
+                "tags":["client:ruby"]}
+              ]
+          }' \
+      'https://app.datadoghq.com/api/v1/series?api_key=#{@api_key}'
+    BASH
+    `#{curl}`
   end
 end
--- src/yagnats_apcera_client/main.go
+++ src/yagnats_apcera_client/main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"os/exec"
+	"regexp"
 	"strings"
 	"sync"
 	"time"
@@ -26,6 +28,21 @@ type Config struct {
 	Name                     string            `yaml:"name"`
 	PayloadSizeInBytes       int               `yaml:"payload_size_in_bytes"`
 	NatsServers              []NatsInformation `yaml:"nats_servers"`
+	Population               int               `yaml:"population"`
+	APIKey                   string            `yaml:"datadog_api_key"`
+}
+
+func dedup(xs *[]string) {
+	found := make(map[string]bool)
+	j := 0
+	for i, x := range *xs {
+		if !found[x] {
+			found[x] = true
+			(*xs)[j] = (*xs)[i]
+			j++
+		}
+	}
+	*xs = (*xs)[:j]
 }
 
 func main() {
@@ -55,48 +72,77 @@ func main() {
 		panic(err.Error())
 	}
 
-	received := map[string]int{}
+	messageTally := map[string][]string{}
+	population := config.Population
+	msgsCompleted := 0
 	m := sync.Mutex{}
 	client.Subscribe(">", func(msg *nats.Msg) {
-		messageParts := strings.Split(string(msg.Data), "--")
-			if len(messageParts) < 2 {
-				return
-			}
-		messageType := messageParts[0]
-		messageName := messageParts[1]
-		if messageType == "publish" {
-			m.Lock()
-			if _, found := received[messageName]; !found {
-				received[messageName] = 0
-			}
-			received[messageName] += 1
-			m.Unlock()
-			output := []string{}
-			for k, v := range received {
-				output = append(output, fmt.Sprintf("%s: %d", k, v))
-			}
-			logger.Info(strings.Join(output, " "))
+		if matched, _ := regexp.Match("^publish--", msg.Data); matched {
+			publishMessage := []byte(fmt.Sprintf("received_publish--%s--%s", config.Name, msg.Data))
+			client.Publish("yagnats.apcera.publish", publishMessage)
 		}
-		// logger.Info(fmt.Sprintf("receiving %s\n", msg.Payload))
+
+		r := regexp.MustCompile(fmt.Sprintf("^received_publish--.*?--publish--%s", config.Name))
+		if r.Match(msg.Data) {
+			m.Lock()
+			r := regexp.MustCompile("^received_publish--(.*?)--(.*)")
+			matches := r.FindSubmatch(msg.Data)
+			originalMsg := string(matches[2])
+			recepient := string(matches[1])
+			if _, found := messageTally[originalMsg]; !found {
+				messageTally[originalMsg] = []string{}
+			}
+			messageTally[originalMsg] = append(messageTally[originalMsg], recepient)
+			stuff := messageTally[originalMsg]
+			dedup(&stuff)
+			messageTally[originalMsg] = stuff
+			if len(messageTally[originalMsg]) == population {
+				logger.Info(fmt.Sprintf("tallied up %s\n", strings.Join(messageTally[originalMsg], ",")))
+				msgsCompleted++
+				delete(messageTally, originalMsg)
+			}
+			m.Unlock()
+		}
 	})
 
 	count := 0
 	for {
 		publishMessage := []byte(fmt.Sprintf("publish--%s--%d--", config.Name, count))
-		publishRequestMessage := []byte(fmt.Sprintf("request--%s--%d--", config.Name, count))
+		//publishRequestMessage := []byte(fmt.Sprintf("request--%s--%d--", config.Name, count))
 
 		publishMessage = padMessage(publishMessage, config.PayloadSizeInBytes)
-		publishRequestMessage = padMessage(publishRequestMessage, config.PayloadSizeInBytes)
+		//publishRequestMessage = padMessage(publishRequestMessage, config.PayloadSizeInBytes)
 
 		logger.Info(fmt.Sprintf("publishing %s\n", publishMessage))
+		logger.Info(fmt.Sprintf("completed %d messages. outstanding: %d", msgsCompleted, len(messageTally)))
+		datadog(config, "msgs.sent", count)
+		datadog(config, "msgs.completed", msgsCompleted)
+		datadog(config, "msgs.outstanding", len(messageTally))
 		client.Publish("yagnats.apcera.publish", publishMessage)
 
-		logger.Info(fmt.Sprintf("requesting %s\n", publishRequestMessage))
-		client.PublishRequest("yagnats.apcera.request", "yagnats.apcera.reply", publishRequestMessage)
+		//logger.Info(fmt.Sprintf("requesting %s\n", publishRequestMessage))
+		//client.PublishRequest("yagnats.apcera.request", "yagnats.apcera.reply", publishRequestMessage)
 
 		count++
 		time.Sleep(time.Duration(config.PublishIntervalInSeconds * float64(time.Second)))
 	}
+}
+
+func datadog(config Config, metric string, value interface{}) {
+	curl := `
+		curl -s -X POST -H "Content-type: application/json" \
+		-d '{ "series" :
+						 [{"metric":"nats-stress.` + metric + `",
+							"points":[[` + fmt.Sprintf("%d", time.Now().UTC().Unix()) + `, ` + fmt.Sprintf("%v", value) + `]],
+							"type":"gauge",
+							"host":"` + config.Name + `",
+							"tags":["client:yagnats-apcera"]}
+						]
+				}' \
+		'https://app.datadoghq.com/api/v1/series?api_key=` + config.APIKey + `'
+	`
+	cmd := exec.Command("bash", "-c", curl)
+	cmd.Run()
 }
 
 func padMessage(message []byte, paddingLength int) []byte {
@@ -111,9 +157,9 @@ func padMessage(message []byte, paddingLength int) []byte {
 	return message
 }
 
-func InitConfigFromFile(path string) *Config {
-	var c *Config = new(Config)
+func InitConfigFromFile(path string) Config {
 	var e error
+	c := Config{}
 
 	b, e := ioutil.ReadFile(path)
 	if e != nil {
