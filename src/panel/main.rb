71d564e72b48d2fba75f232bb3931a1d6d91ee46
Jeff Hui and Serguei Filimonov <sfilimonov@pivotal.io>
2014-11-13 09:20:45 -0800

    add more dials for tc command

--- src/panel/main.rb
+++ src/panel/main.rb
@@ -2,6 +2,11 @@ require 'sinatra'
 
 set :bind, '0.0.0.0'
 
+NATS_IPS = [
+  '10.10.19.101',
+  '10.10.81.101',
+]
+
 def run_commands(commands)
   output = {}
   commands.each do |command|
@@ -43,6 +48,32 @@ get '/dropmo/:interface/:port/:rate' do
     "tc qdisc add dev #{params[:interface]} parent 1:11 netem loss #{params[:rate].to_f * 100.0}%",
     "tc filter add dev #{params[:interface]} protocol ip prio 1 u32 match ip dport #{params[:port]} 0xffff flowid 1:11",
   ]
+  run_commands(commands)
+end
+
+get '/slowmo_client/:interface/:time' do
+  commands = [
+    "tc qdisc del dev #{params[:interface]} root",
+    "tc qdisc add dev #{params[:interface]} handle 1: root htb",
+    "tc class add dev #{params[:interface]} parent 1: classid 1:11 htb rate 4gbps",
+    "tc qdisc add dev #{params[:interface]} parent 1:11 netem delay #{params[:time]}",
+  ]
+  NATS_IPS.each do |ip|
+    commands << "tc filter add dev #{params[:interface]} protocol ip prio 1 u32 match ip dst #{ip} flowid 1:11"
+  end
+  run_commands(commands)
+end
+
+get '/dropmo_client/:interface/:rate' do
+  commands = [
+    "tc qdisc del dev #{params[:interface]} root",
+    "tc qdisc add dev #{params[:interface]} handle 1: root htb",
+    "tc class add dev #{params[:interface]} parent 1: classid 1:11 htb rate 4gbps",
+    "tc qdisc add dev #{params[:interface]} parent 1:11 netem loss #{params[:rate].to_f * 100.0}%",
+  ]
+  NATS_IPS.each do |ip|
+    commands << "tc filter add dev #{params[:interface]} protocol ip prio 1 u32 match ip dst #{ip} flowid 1:11"
+  end
   run_commands(commands)
 end
 
