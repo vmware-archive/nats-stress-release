71d672864f7b46ac2b1ec5a4e90d6d590aa0c1b3
Zach Robinson <zrobinson@pivotal.io>
2014-11-20 11:51:29 -0800

    complete socket->http move

--- src/yagnats_og_client/main.go
+++ src/yagnats_og_client/main.go
@@ -28,20 +28,6 @@ type Config struct {
 	NatsServers              []NatsInformation `yaml:"nats_servers"`
 	Population               int               `yaml:"population"`
 	APIKey                   string            `yaml:"datadog_api_key"`
-	Socket                   string            `yaml:"socket"`
-}
-
-func dedup(xs *[]string) {
-	found := make(map[string]bool)
-	j := 0
-	for i, x := range *xs {
-		if !found[x] {
-			found[x] = true
-			(*xs)[j] = (*xs)[i]
-			j++
-		}
-	}
-	*xs = (*xs)[:j]
 }
 
 var config Config
@@ -53,7 +39,8 @@ func main() {
 
 	c := &gosteno.Config{
 		Sinks: []gosteno.Sink{
-			gosteno.NewFileSink("/var/vcap/sys/log/yagnats_og_client/yagnats_og_client.log"),
+//			gosteno.NewFileSink("/var/vcap/sys/log/yagnats_og_client/yagnats_og_client.log"),
+			gosteno.NewFileSink("/tmp/yagnats_og_client.log"),
 		},
 		Level:     gosteno.LOG_INFO,
 		Codec:     gosteno.NewJsonCodec(),
@@ -93,102 +80,24 @@ func main() {
 
 	for {
 		publishMessage := []byte(fmt.Sprintf("publish--%s--%d--", config.Name, count))
-		// publishWithReplyMessage := []byte(fmt.Sprintf("request--%s--%d--", config.Name, count))
-
 		publishMessage = padMessage(publishMessage, config.PayloadSizeInBytes)
-		// publishWithReplyMessage = padMessage(publishWithReplyMessage, config.PayloadSizeInBytes)
 
-		// logger.Info(fmt.Sprintf("publishing %s\n", publishMessage))
-		// logger.Info(fmt.Sprintf("completed %d messages. outstanding: %d", msgsCompleted, len(messageTally)))
 		client.Publish("yagnats.og.publish", publishMessage)
 		err := communicateMetric([]byte(fmt.Sprintf("sent---%s", string(publishMessage))))
 		if err != nil {
 			logger.Error(err.Error())
 		}
 
-		// logger.Info(fmt.Sprintf("requesting %s\n", publishWithReplyMessage))
-		// client.PublishWithReplyTo("yagnats.og.request", "yagnats.og.reply", publishWithReplyMessage)
-
 		count++
 		time.Sleep(time.Duration(config.PublishIntervalInSeconds * float64(time.Second)))
 	}
 }
 
-// func communicateMetric(message []byte) {
-// 	c, err := net.DialTimeout("unix", config.Socket, 10*time.Second)
-// 	processNetErr(err)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer c.Close()
-// 	_, err = c.Write(message)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
 func communicateMetric(message []byte) error {
-	//c, err := net.DialTimeout("unix", config.Socket, 10*time.Second)
 	resp, err := http.Post("http://127.0.0.1:4568/messages", "application/text", bytes.NewReader(message))
 	resp.Body.Close()
 	return err
-	//defer c.Close()
-	//_, err = c.Write(message)
-	// if err != nil {
-	// 	panic(err)
-	// }
 }
-
-// func processNetErr(err error) {
-// 	if err != nil {
-
-// 		// print error string e.g.
-// 		// "read tcp example.com:80: resource temporarily unavailable"
-// 		fmt.Printf("reader %v\n", err)
-
-// 		// print type of the error, e.g. "*net.OpError"
-// 		fmt.Printf("%T", err)
-
-// 		//if err == os.EINVAL {
-// 		// socket is not valid or already closed
-// 		//fmt.Println("EINVAL")
-// 		//}
-// 		// if err == os.EOF {
-// 		// 	// remote peer closed socket
-// 		// 	fmt.Println("EOF")
-// 		// }
-
-// 		// matching rest of the codes needs typecasting, errno is
-// 		// wrapped on OpError
-// 		if e, ok := err.(*net.OpError); ok {
-// 			// print wrapped error string e.g.
-// 			// "os.Errno : resource temporarily unavailable"
-// 			fmt.Printf("%T : %v\n", e.Error, e.Error)
-// 			if e.Timeout() {
-// 				// is this timeout error?
-// 				fmt.Println("TIMEOUT")
-// 			}
-// 			if e.Temporary() {
-// 				// is this temporary error?  True on timeout,
-// 				// socket interrupts or when buffer is full
-// 				fmt.Println("TEMPORARY")
-// 			}
-
-// 			// specific granular error codes in case we're interested
-// 			// switch e.Error {
-// 			// case os.EAGAIN:
-// 			// 	// timeout
-// 			// 	fmt.Println("EAGAIN")
-// 			// case os.EPIPE:
-// 			// 	// broken pipe (e.g. on connection reset)
-// 			// 	fmt.Println("EPIPE")
-// 			// default:
-// 			// 	// just write raw errno code, can be platform specific
-// 			// 	// (see syscall for definitions)
-// 			// 	fmt.Printf("%d\n", int64(e.Error.(os.Errno)))
-// 			// }
-// 		}
-// 	}
-// }
 
 func padMessage(message []byte, paddingLength int) []byte {
 	if len(message) < paddingLength {
